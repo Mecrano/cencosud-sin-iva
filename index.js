2872a0a3c6188347ff6d6952dbdcf60f12933171
Edwin Alexander Obando <edwin.obando@itglobers.com>
2020-06-15 14:54:20 -0500

    change port for heroku

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.post("/consultaEan", jsonParser, (req, res) => {
     }
 });
 
-app.listen(3000, function () {
-    console.log("app listening on port 3000!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
